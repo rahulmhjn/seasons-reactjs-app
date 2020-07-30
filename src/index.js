d54b9baa6bd3d2475243912597943aa3ee2e3c7c
rahulmhjn <rahul.mahajan676@gmail.com>
2020-07-30 15:58:58 +0530

    class component and setState

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return <div>Hi!</div>;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // THIS IS THE ONLY TIME we do direct assignment to this.state
+    this.state = { lat: 40 };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        // we called setState
+        this.setState({ lat: position.coords.latitude });
+
+        //we did not!!!
+        // this.state.lat = position.coords.latitude
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  // React says we have to return render!!
+  render() {
+    return <div>Latitude: {this.state.lat} </div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
